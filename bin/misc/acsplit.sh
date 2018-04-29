4c6d07bec7f326f81f14e9818dba8c5b1c682440
ns408 <nsrea408@gmail.com>
2018-04-30 07:37:09 +1000

    Add misc scripts

---
+++ bin/misc/acsplit.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ -z ${1+x} || -z ${2+x} ]]; then
+  cat >&2 <<HELP
+./acsplit KMD 50
+./acsplit BTC 50
+HELP
+  exit 1
+fi
+
+curl --url "http://127.0.0.1:7776" --data "{\"coin\":\""${1}"\",\"agent\":\"iguana\",\"method\":\"splitfunds\",\"satoshis\":\"10000\",\"sendflag\":1,\"duplicates\":"${2}"}"
