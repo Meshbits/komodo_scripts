b270777075a0edd263f9d7c47475a3acfdeadcb6
ns408 <nsrea408@gmail.com>
2018-04-29 15:14:01 +1000

    Freaking tab

--- bin/.komodo/bin/start.sh
+++ bin/.komodo/bin/start.sh
@@ -3,10 +3,10 @@ set -e
 
 if ! $( lsof -Pi :<VAR_RPCPORT> -sTCP:LISTEN -t > /dev/null); then
   while [[ $# -gt 0 ]]; do
-  	key="$1"
-  	case $key in
-  		-h|--help)
-  		cat >&2 <<HELP
+    key="$1"
+    case $key in
+      -h|--help)
+      cat >&2 <<HELP
 Usage: start.sh [OPTIONS]
 Start komodod
 -h --help                         Show this help
