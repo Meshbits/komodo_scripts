e03fdc67c7913e80766635e141571b550624d691
ns408 <nsrea408@gmail.com>
2018-04-30 02:21:42 +1000

    Incorrect parameter rectified

--- bin/.bitcoin/bin/status.sh
+++ bin/.bitcoin/bin/status.sh
@@ -4,7 +4,7 @@ set -e
 count=0
 while [[ count -lt 300 ]]; do
   if "<VAR_SRC_DIR>/src/bitcoin-cli" getblockchaininfo >& /dev/null; then
-    getblockchaininfo="$(<VAR_SRC_DIR>/src/bitcoin-cli getinfo)"
+    getblockchaininfo="$(<VAR_SRC_DIR>/src/bitcoin-cli getblockchaininfo)"
     if [[ $(echo $getblockchaininfo | jq -r .blocks) -eq $(echo $getblockchaininfo | jq -r .headers) ]]; then
       echo -e '## Bitcoin blockchain in sync with the network ##'
       break
--- bin/.chips/bin/status.sh
+++ bin/.chips/bin/status.sh
@@ -4,7 +4,7 @@ set -e
 count=0
 while [[ count -lt 300 ]]; do
   if "<VAR_SRC_DIR>/src/chips-cli" getblockchaininfo >& /dev/null; then
-    getblockchaininfo="$(<VAR_SRC_DIR>/src/chips-cli getinfo)"
+    getblockchaininfo="$(<VAR_SRC_DIR>/src/chips-cli getblockchaininfo)"
     if [[ $(echo $getblockchaininfo | jq -r .blocks) -eq $(echo $getblockchaininfo | jq -r .headers) ]]; then
       echo -e '## Chips blockchain in sync with the network ##'
       break
