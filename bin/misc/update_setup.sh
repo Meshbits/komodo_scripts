8e9a3a7e1b64b5d95adb3a1faf71d2018ac96900
ns408 <nsrea408@gmail.com>
2018-04-30 10:45:46 +1000

    Script to update the komodo_scripts

---
+++ bin/misc/update_setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo -s bash <<EOF
+cd /usr/local/src
+if [[ -d komodo_scripts ]]; then
+  cd komodo_scripts
+  git checkout master; git reset --hard; git pull --rebase
+  cd - >& /dev/null
+else
+  git clone https://github.com/ns408/komodo_scripts.git
+fi
+EOF
