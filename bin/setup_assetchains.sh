14591f454da96c80a20b58d6f02e16da2add5b7c
ns408 <nsrea408@gmail.com>
2018-05-05 16:53:51 +1000

    Setup assetchain control scripts

---
+++ bin/setup_assetchains.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -e
+
+echo -e "## Komodod Assetchains setup starting ##\n"
+
+# Setup configuration which monit can use
+sed -e "s|<HOME>|${HOME}|g" \
+  -e "s|<EXTERNALIP>|${EXTERNALIP}|g" \
+  $(dirname $0)/.komodo/bin/ac_start.sh > ${HOME}/.komodo/bin/ac_start.sh
+
+sed -e "s|<HOME>|${HOME}|" \
+  $(dirname $0)/.komodo/bin/ac_status.sh > ${HOME}/.komodo/bin/ac_status.sh
+
+sed -e "s|<HOME>|${HOME}|" \
+  $(dirname $0)/.komodo/bin/ac_stop.sh > ${HOME}/.komodo/bin/ac_stop.sh
+
+# Setup configuration which monit can use
+sed -e "s|<HOME>|${HOME}|" \
+  $(dirname $0)/.komodo/bin/ac_healthcheck.sh > ${HOME}/.komodo/bin/ac_healthcheck.sh
+
+# Create monit template
+cat > ${HOME}/.komodo/monitd_assetchains.template <<EOF
+check program assetchains_healthcheck.sh with path "${HOME}/.komodo/bin/ac_healthcheck.sh"
+  as uid ${USER} and gid ${USER}
+  with timeout 60 seconds
+if status != 0 then exec "${HOME}/.komodo/bin/ac_start.sh"
+  as uid ${USER} and gid ${USER}
+  repeat every 2 cycles
+EOF
+
+# Copy monit configuration
+sudo mv ${HOME}/.komodo/monitd_assetchains.template /etc/monit/conf.d/monitd_assetchains
+sudo chmod 600 /etc/monit/conf.d/monitd_assetchains
+
+# Permissions and ownership
+chmod +x ${HOME}/.komodo/bin/*
+chmod 600 ${HOME}/.komodo/conf/*.conf
+
+echo -e "## Komodod Assetchains have been configured ##\n"
