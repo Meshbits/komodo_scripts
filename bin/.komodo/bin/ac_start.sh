bea60c28295c26cf3a1decf12c6b1db94ed32902
ns408 <nsrea408@gmail.com>
2018-08-08 22:38:59 +1000

    Modifying script to accommodate assetchains.json

--- bin/.komodo/bin/ac_start.sh
+++ bin/.komodo/bin/ac_start.sh
@@ -10,39 +10,37 @@ source /etc/profile
 cd <HOME>/komodo
 seed_ip=$(getent hosts zero.kolo.supernet.org | awk '{ print $1 }')
 EXTERNALIP="-externalip=<EXTERNALIP>"
-ASSETCHAINS_FILE="<HOME>/komodo/src/assetchains"
+ASSETCHAINS_FILE="<HOME>/komodo/src/assetchains.json"
 
 function komodo_asset () {
   <HOME>/komodo/src/komodod -ac_name=$1 -ac_supply=$2 -addnode=$seed_ip -maxconnections=512 ${KOMODO_ASSETCHAINS_STARTUP_OPTIONS} \
     >& ${HOME}/.komodo/log/${name}.log
 }
 
-# Check that we can actually find '^komodo_asset' before doing anything else
-if grep -P '^komodo_asset' ${ASSETCHAINS_FILE} >& /dev/null; then
-  for list in $(grep -P '^komodo_asset' ${ASSETCHAINS_FILE} | awk '{ print $2 ":" $3 }' );
-  do
-    name=$(echo $list | awk -F':' '{ print $1 }')
-    if [[ ${name} == "BEER" || ${name} == "PIZZA" || ${name} == "VOTE2018" ]]; then continue; fi
+for ((item=1; item<$(cat ${ASSETCHAINS_FILE} | jq '. | length'); item++)); do
+do
+  name=$(cat ${ASSETCHAINS_FILE} | jq -r ".[${item}] | .ac_name")
 
-    supply=$(echo $list | awk -F':' '{ print $2 }')
-    conffile=<HOME>/.komodo/${name}/${name}.conf
+  if [[ ${name} == "BEER" || ${name} == "PIZZA" || ${name} == "VOTE2018" ]]; then continue; fi
 
-    if [[ -f ${conffile} ]]; then
-      komodo_asset ${name} ${supply} &
-    else
-      sed -i 's|rpcworkqueue=64|rpcworkqueue=256|' ${conffile}
-      RPCPORT=$(grep 'rpcport=' ${conffile} | cut -d'=' -f2)
-      if ! $( lsof -Pi :${RPCPORT} -sTCP:LISTEN -t >& /dev/null); then
-        if [[ "${EXTERNALIP}" = "-externalip=<EXTERNALIP>" || "${EXTERNALIP}" = "-externalip=" ]]; then
-          komodo_asset ${name} ${supply} &
-        else
-          komodo_asset ${name} ${supply} "${EXTERNALIP}" &
-        fi
+  supply=$(cat ${ASSETCHAINS_FILE} | jq -r ".[${item}] | .ac_supply")
+  conffile=<HOME>/.komodo/${name}/${name}.conf
+
+  if [[ -f ${conffile} ]]; then
+    komodo_asset ${name} ${supply} &
+  else
+    sed -i 's|rpcworkqueue=64|rpcworkqueue=256|' ${conffile}
+    RPCPORT=$(grep 'rpcport=' ${conffile} | cut -d'=' -f2)
+    if ! $( lsof -Pi :${RPCPORT} -sTCP:LISTEN -t >& /dev/null); then
+      if [[ "${EXTERNALIP}" = "-externalip=<EXTERNALIP>" || "${EXTERNALIP}" = "-externalip=" ]]; then
+        komodo_asset ${name} ${supply} &
+      else
+        komodo_asset ${name} ${supply} "${EXTERNALIP}" &
       fi
     fi
-    sleep 1
-  done
-fi
+  fi
+  sleep 1
+done
 
 # Go back to the previous directory
 cd - >& /dev/null
