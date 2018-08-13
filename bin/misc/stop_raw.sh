3ec41f1f22d4533a54b2adfa26a6894c40586000
ns408 <nsrea408@gmail.com>
2018-08-13 23:44:31 +1000

    Update the main startup and stop script

--- bin/misc/stop_raw.sh
+++ bin/misc/stop_raw.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 sudo /etc/init.d/monit stop
-#~/.bitcoin/bin/stop.sh
+~/.bitcoin/bin/stop.sh
 ~/.komodo/bin/stop.sh
 ~/.komodo/bin/ac_stop.sh
 ~/.chips/bin/stop.sh
 ~/.gamecredits/bin/stop.sh
+~/.komodo/VRSC/bin/stop.sh
 
 pkill -15 iguana
