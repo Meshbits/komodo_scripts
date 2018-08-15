07ffa0f8f6bd3778defbcefa14e57a5b402155a6
ns408 <nsrea408@gmail.com>
2018-08-16 02:05:51 +1000

    Fixing a major f-up with autosplit script.

--- bin/misc/acsplit.sh
+++ bin/misc/acsplit.sh
@@ -9,6 +9,11 @@ HELP
   exit 1
 fi
 
-satoshis=10000
+if [[ -z ${3+x} ]]; then
+  satoshis=10000
+else
+  satoshis=${3}
+fi
+
 curl --url "http://127.0.0.1:7776" \
   --data "{\"coin\":\"${1}\",\"agent\":\"iguana\",\"method\":\"splitfunds\",\"satoshis\":\"${satoshis}\",\"sendflag\":1,\"duplicates\":\"${2}\"}"
--- bin/misc/cron_recharge_utxos.sh
+++ bin/misc/cron_recharge_utxos.sh
@@ -5,46 +5,52 @@ set -e
 source /etc/profile
 [[ -f "${HOME}/.common/config" ]] && source "${HOME}/.common/config"
 
-dsatoshis='.0001'
+dsatoshis='.00010000'
+dsatoshis_gamecredits='.00100000'
 
 if [[ $(bitcoin-cli listunspent | grep ${dsatoshis} | wc -l) -lt 20 ]]; then
-  /usr/local/bin/slack_alert "$(echo -n -e 'BTC utxos before split:\t'; bitcoin-cli listunspent | grep ${dsatoshis} | wc -l)"
+  /usr/local/bin/slack_alert testing "$(echo -n -e 'BTC utxos before split:\t'; bitcoin-cli listunspent | grep ${dsatoshis} | wc -l)"
   ${HOME}/misc_scripts/acsplit.sh BTC 20
-  /usr/local/bin/slack_alert "$(echo -n -e 'BTC utxos after split:\t'; bitcoin-cli listunspent | grep ${dsatoshis} | wc -l)"
+  /usr/local/bin/slack_alert testing "$(echo -n -e 'BTC utxos after split:\t'; bitcoin-cli listunspent | grep ${dsatoshis} | wc -l)"
 fi
 
 if [[ $(chips-cli listunspent | grep ${dsatoshis} | wc -l) -lt 20 ]]; then
-  /usr/local/bin/slack_alert "$(echo -n -e 'CHIPS utxos before split:\t'; chips-cli listunspent | grep ${dsatoshis} | wc -l)"
+  /usr/local/bin/slack_alert testing "$(echo -n -e 'CHIPS utxos before split:\t'; chips-cli listunspent | grep ${dsatoshis} | wc -l)"
   ${HOME}/misc_scripts/acsplit.sh CHIPS 20
-  /usr/local/bin/slack_alert "$(echo -n -e 'CHIPS utxos after split:\t'; chips-cli listunspent | grep ${dsatoshis} | wc -l)"
+  /usr/local/bin/slack_alert testing "$(echo -n -e 'CHIPS utxos after split:\t'; chips-cli listunspent | grep ${dsatoshis} | wc -l)"
 fi
 
-if [[ $(gamecredits-cli listunspent | grep ${dsatoshis} | wc -l) -lt 20 ]]; then
-  /usr/local/bin/slack_alert "$(echo -n -e 'GameCredits utxos before split:\t'; gamecredits-cli listunspent | grep ${dsatoshis} | wc -l)"
-  ${HOME}/misc_scripts/acsplit.sh GAME 5
-  /usr/local/bin/slack_alert "$(echo -n -e 'GameCredits utxos after split:\t'; gamecredits-cli listunspent | grep ${dsatoshis} | wc -l)"
+if [[ $(gamecredits-cli listunspent | grep ${dsatoshis_gamecredits} | wc -l) -lt 20 ]]; then
+  /usr/local/bin/slack_alert testing "$(echo -n -e 'GameCredits utxos before split:\t'; gamecredits-cli listunspent | grep ${dsatoshis} | wc -l)"
+  ${HOME}/misc_scripts/acsplit.sh GAME 10 100000
+  /usr/local/bin/slack_alert testing "$(echo -n -e 'GameCredits utxos after split:\t'; gamecredits-cli listunspent | grep ${dsatoshis} | wc -l)"
 fi
 
 if [[ $(komodo-cli listunspent | grep ${dsatoshis} | wc -l) -lt 20 ]]; then
-  /usr/local/bin/slack_alert "$(echo -n -e 'KMD utxos before split:\t'; komodo-cli listunspent | grep ${dsatoshis} | wc -l)"
+  /usr/local/bin/slack_alert testing "$(echo -n -e 'KMD utxos before split:\t'; komodo-cli listunspent | grep ${dsatoshis} | wc -l)"
   ${HOME}/misc_scripts/acsplit.sh KMD 20
-  /usr/local/bin/slack_alert "$(echo -n -e 'KMD utxos after split:\t'; komodo-cli listunspent | grep ${dsatoshis} | wc -l)"
+  /usr/local/bin/slack_alert testing "$(echo -n -e 'KMD utxos after split:\t'; komodo-cli listunspent | grep ${dsatoshis} | wc -l)"
 fi
 
-ASSETCHAINS_FILE="${HOME}/komodo/src/assetchains"
-
-# Check that we can actually find '^komodo_asset' before doing anything else
-if grep -P '^komodo_asset' ${ASSETCHAINS_FILE} >& /dev/null; then
-  for name in $(grep -P '^komodo_asset' ${ASSETCHAINS_FILE} | awk '{ print $2 }' );
-  do
-    if [[ ${name} == "BEER" || ${name} == "PIZZA" || ${name} == "VOTE2018" ]]; then continue; fi
-
-    if [[ $(komodo-cli -ac_name=${name} listunspent | grep ${dsatoshis} | wc -l) -lt 20 ]]; then
-      /usr/local/bin/slack_alert \
-        "$(echo -n ${name}; echo -n -e ' utxos before split:\t'; komodo-cli listunspent | grep ${dsatoshis} | wc -l)"
-      ${HOME}/misc_scripts/acsplit.sh ${name} 20
-      /usr/local/bin/slack_alert \
-        "$(echo -n ${name}; echo -n -e ' utxos after split:\t'; komodo-cli listunspent | grep ${dsatoshis} | wc -l)"
-    fi
-  done
+if [[ $(${HOME}/veruscoin/src/komodo-cli -ac_name=VRSC listunspent | grep ${dsatoshis} | wc -l) -lt 10 ]]; then
+  /usr/local/bin/slack_alert testing "$(echo -n -e 'VRSC utxos before split:\t'; komodo-cli listunspent | grep ${dsatoshis} | wc -l)"
+  ${HOME}/misc_scripts/acsplit.sh VRSC 20
+  /usr/local/bin/slack_alert testing "$(echo -n -e 'VRSC utxos after split:\t'; komodo-cli listunspent | grep ${dsatoshis} | wc -l)"
 fi
+
+
+ASSETCHAINS_FILE="<HOME>/komodo/src/assetchains.json"
+
+for ((item=0; item<$(cat ${ASSETCHAINS_FILE} | jq '. | length'); item++));
+do
+  name=$(cat ${ASSETCHAINS_FILE} | jq -r ".[${item}] | .ac_name")
+  if [[ ${name} == "BEER" || ${name} == "PIZZA" || ${name} == "VOTE2018" ]]; then continue; fi
+
+  if [[ $(komodo-cli -ac_name=${name} listunspent | grep ${dsatoshis} | wc -l) -lt 20 ]]; then
+    /usr/local/bin/slack_alert testing \
+      "$(echo -n ${name}; echo -n -e ' utxos before split:\t'; komodo-cli listunspent | grep ${dsatoshis} | wc -l)"
+    ${HOME}/misc_scripts/acsplit.sh ${name} 20
+    /usr/local/bin/slack_alert testing \
+      "$(echo -n ${name}; echo -n -e ' utxos after split:\t'; komodo-cli listunspent | grep ${dsatoshis} | wc -l)"
+  fi
+done
