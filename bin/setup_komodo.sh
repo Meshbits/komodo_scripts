4aa7ef059fc9ad40c38bb5d8180727b7301f450b
ns408 <nsrea408@gmail.com>
2018-08-18 07:34:37 +1000

    Update

--- bin/setup_komodo.sh
+++ bin/setup_komodo.sh
@@ -14,7 +14,7 @@ source /etc/profile
 # Functions
 # Capture real time taken
 function time_taken() {
-  time -f "## Time taken=%e\n" "$@"
+  /usr/bin/time -f "## Time taken=%e\n" "$@"
 }
 
 # Variables
