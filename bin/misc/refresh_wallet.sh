1a03df0fa1f75b5283abc880ba9572eda4e9c119
ns408 <nsrea408@gmail.com>
2018-09-09 17:10:08 +1000

    Less talk

--- bin/misc/refresh_wallet.sh
+++ bin/misc/refresh_wallet.sh
@@ -42,6 +42,7 @@ fi
 
 # save the privkey to a variable so we import it later
 NN_private_key=$(komodo-cli dumpprivkey ${NN_KOMODO_ADDRESS} | tee ${HOME}/.temp_sensitive/nn_komodo_key)
+TEMP_private_key=$(komodo-cli dumpprivkey ${TEMP_KOMODO_ADDRESS} | tee ${HOME}/.temp_sensitive/temp_komodo_key)
 
 # What da balance
 balance=$(komodo-cli getbalance)
@@ -50,9 +51,13 @@ balance_minus_ten=$(bc <<< "$balance-10.0")
 # send all but 10 komodo to VAULT_KOMODO_ADDRESS
 komodo-cli sendtoaddress "${VAULT_KOMODO_ADDRESS}" ${balance_minus_ten} "" "" true
 
+# stop monit or it'll start the nn
+sudo /etc/init.d/monit stop
+
 # Stop komodo and start without -gen
 komodo-cli stop
-komodod &; sleep 100
+komodod &
+sleep 60
 ~/.komodo/bin/status.sh
 
 # send all komodo to TEMP_KOMODO_ADDRESS
@@ -72,10 +77,22 @@ komodo-cli sendtoaddress "${TEMP_KOMODO_ADDRESS}" $(komodo-cli getbalance) "" ""
 
 mv ~/.komodo/{wallet.dat,_`date +%s`}
 
+komodod &
+sleep 60
+~/.komodo/bin/status.sh
+
 #bitcoin-cli importprivkey "Kxxx" "" false
 komodo-cli importprivkey $(cat ${HOME}/.temp_sensitive/nn_komodo_key) "" false
+komodo-cli importprivkey $(cat ${HOME}/.temp_sensitive/temp_komodo_key) "temp_vault" false
 
-komodod &; sleep 100
-~/.komodo/bin/status.sh
+# send komodo from temp_vault to nn_komodo_address minus transaction fee
+temp_vault_balance=$(komodo-cli getbalance temp_vault)
+temp_vault_balance_minus_trans=$(bc <<< "$temp_vault_balance-0.001")
+komodo-cli sendmany "temp_vault" "{\"${NN_KOMODO_ADDRESS}\":\"$temp_vault_balance_minus_trans\"}"
+
 ~/.komodo/bin/stop.sh
 ~/.komodo/bin/start.sh &
+
+~/.komodo/bin/status.sh
+~/misc_scripts/cron_recharge_utxos.sh
+sudo /etc/init.d/monit start
