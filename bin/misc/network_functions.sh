3d967b8220594c95b6f93b8810874d771c812103
ns408 <nsrea408@gmail.com>
2018-10-13 06:08:38 +1100

    Update functions

--- bin/misc/network_functions.sh
+++ bin/misc/network_functions.sh
@@ -3,19 +3,25 @@
 #sudo apt -y install parellel
 #parallel --gnu ping -c1 ::: `dig +trace google.com|ipx`
 
-function nn_ping() {
+function nn_connected_ip() {
+  curl -s --url "http://127.0.0.1:7776/" --data "{\"agent\":\"dpow\",\"method\":\"ipaddrs\"}" | jq -r .[]
+}
+
+function func_nn_connect() {
+  # this is probably not needed and can be replaced by nn_connected_ip()
   grep NN_CONNECT ${HOME}/start_raw.log \
-    | grep -o '[0-9]\+[.][0-9]\+[.][0-9]\+[.][0-9]\+' | uniq \
-    | while read variable
+    | grep -o '[0-9]\+[.][0-9]\+[.][0-9]\+[.][0-9]\+' | uniq
+}
+
+function nn_ping() {
+  func_nn_connect | while read variable
   do
     nc -zn -w2 ${variable} 17775 || echo "Unable to connect to ${variable}" &
   done
 }
 
 function nn_ufw() {
-  grep NN_CONNECT ${HOME}/start_raw.log \
-    | grep -o '[0-9]\+[.][0-9]\+[.][0-9]\+[.][0-9]\+' | uniq \
-    | while read variable
+  func_nn_connect | while read variable
   do
     sudo grep ${variable} /var/log/ufw.log | uniq
   done
