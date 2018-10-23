e16197ea3c36d9308e0689d759f1f8ab76319f33
ns408 <nsrea408@gmail.com>
2018-10-24 02:39:54 +1100

    add docker

---
+++ docker_run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+export var_tag="$1"
+
+function image_build() {
+  #docker build - < Dockerfile_komodo
+  docker build -f Dockerfile_komodo -t "meshbits_komodo1604:${var_tag}" .
+}
+
+function image_push() {
+  docker build -f Dockerfile_komodo -t "meshbits_komodo1604:${var_tag}" .
+  docker tag "meshbits_komodo1604:${var_tag}" "nsrea408/meshbits_komodo1604:${var_tag}"
+  docker push "nsrea408/meshbits_komodo1604:${var_tag}"
+}
