d988bf2693a4970c5a12cba96ebea45b1bb98196
ns408 <nsrea408@gmail.com>
2018-11-14 14:15:30 +1100

    update

---
+++ bin/misc/functions.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Shared functions etc.
+
+#--------------------------------------------------------------------------------
+# Reference: https://stackoverflow.com/questions/1715137/what-is-the-best-way-to-ensure-only-one-instance-of-a-bash-script-is-running
+LOCKFILE="/var/lock/`basename $0`"
+LOCKFD=99
+
+# PRIVATE
+_lock()             { flock -$1 $LOCKFD; }
+_no_more_locking()  { _lock u; _lock xn && rm -f $LOCKFILE; }
+_prepare_locking()  { eval "exec $LOCKFD>\"$LOCKFILE\""; trap _no_more_locking EXIT; }
+
+# ON START
+_prepare_locking
+
+# PUBLIC
+exlock_now()        { _lock xn; }  # obtain an exclusive lock immediately or fail
+exlock()            { _lock x; }   # obtain an exclusive lock
+shlock()            { _lock s; }   # obtain a shared lock
+unlock()            { _lock u; }   # drop a lock
+
+### BEGIN OF SCRIPT ###
+
+# Simplest example is avoiding running multiple instances of script.
+#exlock_now || exit 1
+#--------------------------------------------------------------------------------
