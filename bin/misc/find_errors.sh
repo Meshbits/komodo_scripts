7785504fe621a9b732ec7766b153772b8f056d32
ns408 <nsrea408@gmail.com>
2018-11-18 05:35:00 +1100

    update

--- bin/misc/find_errors.sh
+++ bin/misc/find_errors.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 DEBUGLOG_ERRORS="${HOME}/debuglog_errors"
-DAEMON_ERRORS="${HOME}/daemon_errors"
 AC_DAEMON_ERRORS="${HOME}/ac_daemon_errors"
 STARTUP_ERRORS="${HOME}/startup_errors"
 
@@ -9,10 +8,18 @@ function grep_me() {
   grep -i -P "err|fork|fail|please send funds"
 }
 
+pids=""
+
 rm -rf ${DEBUGLOG_ERRORS} ${DAEMON_ERRORS} ${AC_DAEMON_ERRORS} ${STARTUP_ERRORS}
 
-find ${HOME}/ -iname debug.log -exec tail -f {} \; | grep_me >> ${DEBUGLOG_ERRORS} &
-find ${HOME}/ -iname daemon.log -exec tail -f {} \; | grep_me >>  ${DAEMON_ERRORS} &
+find ${HOME}/ -iname 'debug.log' -o -iname 'daemon.log' \
+  -exec tail -f {} \; | grep_me >> ${DEBUGLOG_ERRORS} &
+pids="$pids $!"
 
-tail -f ${HOME}/*.log | grep_me >> ${STARTUP_ERRORS} &
-tail -f ${HOME}/.komodo/log/*.log | grep_me >> ${STARTUP_ERRORS} &
+tail -f ${HOME}/*.log ${HOME}/.komodo/log/*.log | grep_me >> ${STARTUP_ERRORS} &
+pids="$pids $!"
+
+wait -n ${pids}
+
+trap 'kill ${pids}' SIGINT SIGTERM EXIT
+#trap 'kill $(jobs -p)' SIGINT SIGTERM EXIT
