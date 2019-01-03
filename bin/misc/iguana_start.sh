e05522803553d60b3d4f530404ec1f281195d17c
ns408 <nsrea408@gmail.com>
2019-01-03 20:50:48 +1100

    Overwrite the file

--- bin/misc/iguana_start.sh
+++ bin/misc/iguana_start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 LOG_FILE="${HOME}/iguana.log"
-exec 3>&1 1>>${LOG_FILE} 2>&1
+exec 3>&1 1>${LOG_FILE} 2>&1
 
 if [[ $EUID -eq 0 ]]; then
    echo -e "This script needs to run as a non-root user\n"
