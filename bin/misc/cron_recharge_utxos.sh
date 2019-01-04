6ba0d6eaf6c7b66c68d5ac33196ffb0989ddce52
ns408 <nsrea408@gmail.com>
2019-01-05 06:03:40 +1100

    updated

--- bin/misc/cron_recharge_utxos.sh
+++ bin/misc/cron_recharge_utxos.sh
@@ -97,13 +97,13 @@ ${HOME}/komodo/src/listassetchains | while read item; do
     echo -e "${item} Split"
 
     /usr/local/bin/slack_alert testing \
-      "$(echo -n ${item}; echo -n -e ' utxos before split:\t'; komodo-cli -ac_name=${item} listunspent 10 | grep ${dsatoshis} | wc -l)"
+      "$(echo -n ${item}; echo -n -e ' utxos before split: '; komodo-cli -ac_name=${item} listunspent 10 | grep ${dsatoshis} | wc -l)"
 
     RESULT="$(${HOME}/misc_scripts/acsplit.sh ${item} ${var_value})"
     print_txid "$RESULT"
 
     /usr/local/bin/slack_alert testing \
-      "$(echo -n ${item}; echo -n -e ' utxos after split:\t'; komodo-cli -ac_name=${item} listunspent 10 | grep ${dsatoshis} | wc -l)"
+      "$(echo -n ${item}; echo -n -e ' utxos after split: '; komodo-cli -ac_name=${item} listunspent 1 | grep ${dsatoshis} | wc -l)"
 
     echo -e "\n"
   fi
