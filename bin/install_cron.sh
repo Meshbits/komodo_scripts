25e70404a74001263aa9637d06cd151b7501e251
ns408 <nsrea408@gmail.com>
2019-01-05 06:25:56 +1100

    not too often

--- bin/install_cron.sh
+++ bin/install_cron.sh
@@ -10,7 +10,7 @@ cat > /var/spool/cron/crontabs/meshbits <<\EOF
 MAILTO=''
 */5 * * * * bash /home/meshbits/misc_scripts/cron_recharge_utxos.sh
 00 23 * * * bash /home/meshbits/misc_scripts/send_to_vault.sh
-0 0,6,12,18 * * * bash /home/meshbits/misc_scripts/wallet_reset_kmd.sh
+0 12 * * * bash /home/meshbits/misc_scripts/wallet_reset_kmd.sh
 #0 * * * * bash /usr/local/bin/slack_alert "#nn_reports" "\`\`\`$(echo $(hostname);bash -l /home/meshbits/misc_scripts/stats_new | grep -P -- 'KMD |BTC |-ASSET- ')\`\`\`"
 #*/10 * * * * bash /home/meshbits/misc_scripts/iguana_start.sh
 EOF
