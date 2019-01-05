b37737325398266d1e5079b3be7002a4a272258d
ns408 <nsrea408@gmail.com>
2019-01-06 10:17:42 +1100

    Move code if src exists in home dir

--- bin/misc/update_all_daemons.sh
+++ bin/misc/update_all_daemons.sh
@@ -7,6 +7,17 @@ SCRIPTPATH=$(dirname $SCRIPTNAME)
 
 LOGFILE="${HOME}/start_raw.log"
 
+function move_source() {
+  item="$1"
+  cd ${HOME}
+  [[ -d .build_source/${item}_old ]] && rm -rf .build_source/${item}_old
+
+  if [[ -d .build_source/${item} && -d ${item} ]]; then
+    mv ${item} .build_source/${item}_old
+    mv .build_source/${item} .
+  fi
+}
+
 # source profile and setup variables using "${HOME}/.common/config"
 source /etc/profile
 [[ -f "${HOME}/.common/config" ]] && source "${HOME}/.common/config"
@@ -16,7 +27,7 @@ ${SCRIPTPATH}/start_raw.sh >& ${LOGFILE}
 
 # update the daemons
 echo -e "Starting upgrading everything in .build_source directory\n"
-/usr/local/src/komodo_scripts/bin/setup_komodo.sh
+/usr/local/src/komodo_scripts/bin/setup_komodo.sh && move_source komodo
 /usr/local/src/komodo_scripts/bin/setup_assetchains.sh
 /usr/local/src/komodo_scripts/bin/setup_chips.sh
 /usr/local/src/komodo_scripts/bin/setup_gamecredits.sh
@@ -29,12 +40,6 @@ echo -e "Finished upgrading everything in .build_source directory\n"
 echo -e "Stop all services\n"
 ${SCRIPTPATH}/stop_raw.sh
 
-cd ${HOME}
-for list in komodo; do
-  [[ -d .build_source/${list}_old ]] && rm -rf .build_source/${list}_old
-  [[ -d ${list} ]] && mv ${list} .build_source/${list}_old
-  [[ -d .build_source/${list} ]] && mv .build_source/${list} .
-done
 echo -e "Moved all assets to \$HOME \n"
 
 # Stop and start iguana
