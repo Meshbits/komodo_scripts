2d4c6e41bf93aebc3f14866bc368e45c2af84954
ns408 <nsrea408@gmail.com>
2019-01-06 15:23:36 +1100

    update

--- bin/misc/update_all_daemons.sh
+++ bin/misc/update_all_daemons.sh
@@ -7,6 +7,18 @@ SCRIPTPATH=$(dirname $SCRIPTNAME)
 
 LOGFILE="${HOME}/start_raw.log"
 
+function move_source() {
+  item="$1"
+  cd ${HOME}
+  [[ -d .build_source/${item}_old ]] && rm -rf .build_source/${item}_old
+
+  if [[ -d .build_source/${item} && -d ${item} ]]; then
+    mv ${item} .build_source/${item}_old
+    mv .build_source/${item} .
+  fi
+  echo -e "Moved to \$HOME: ${item} \n"
+}
+
 # source profile and setup variables using "${HOME}/.common/config"
 source /etc/profile
 [[ -f "${HOME}/.common/config" ]] && source "${HOME}/.common/config"
@@ -16,28 +28,21 @@ ${SCRIPTPATH}/start_raw.sh >& ${LOGFILE}
 
 # update the daemons
 echo -e "Starting upgrading everything in .build_source directory\n"
-/usr/local/src/komodo_scripts/bin/setup_komodo.sh
-/usr/local/src/komodo_scripts/bin/setup_chips.sh
-/usr/local/src/komodo_scripts/bin/setup_gamecredits.sh
-/usr/local/src/komodo_scripts/bin/setup_veruscoin.sh
-/usr/local/src/komodo_scripts/bin/setup_hush.sh
-/usr/local/src/komodo_scripts/bin/setup_emc2.sh
+/usr/local/src/komodo_scripts/bin/setup_komodo.sh && move_source komodo
+/usr/local/src/komodo_scripts/bin/setup_chips.sh && move_source chips
+/usr/local/src/komodo_scripts/bin/setup_gamecredits.sh && move_source gamecredits
+/usr/local/src/komodo_scripts/bin/setup_veruscoin.sh && move_source veruscoin
+/usr/local/src/komodo_scripts/bin/setup_hush.sh && move_source hush
+/usr/local/src/komodo_scripts/bin/setup_emc2.sh && move_source einsteinium
 echo -e "Finished upgrading everything in .build_source directory\n"
 
 # Stop all services first
 echo -e "Stop all services\n"
 ${SCRIPTPATH}/stop_raw.sh
 
-cd ${HOME}
-for list in chips gamecredits komodo veruscoin hush einsteinium; do
-  [[ -d .build_source/${list}_old ]] && rm -rf .build_source/${list}_old
-  [[ -d ${list} ]] && mv ${list} .build_source/${list}_old
-  [[ -d .build_source/${list} ]] && mv .build_source/${list} .
-done
 echo -e "Moved all assets to \$HOME \n"
 
 # Stop and start iguana
 ${SCRIPTPATH}/iguana_stop.sh
-${SCRIPTPATH}/iguana_start.sh >& ${LOGFILE} &
 
 ${SCRIPTPATH}/start_raw.sh &>> ${LOGFILE}
