05a1023a832608d9d4758e6f22c586fa292c6e83
ns408 <nsrea408@gmail.com>
2019-01-28 17:32:29 +1100

    update

--- bin/setup_bitcoin.sh
+++ bin/setup_bitcoin.sh
@@ -142,18 +142,22 @@ sed -e "s|<VAR_RPCPORT>|${VAR_RPCPORT}|g" \
   -e "s|<VAR_CONF_FILE>|${VAR_CONF_FILE}|g" \
   -e "s|<VAR_NPROC>|${VAR_NPROC}|g" \
   -e "s|<VAR_USERNAME>|${VAR_USERNAME}|g" \
+  -e "s|<VAR_THING>|${VAR_THING}|g" \
   "${SCRIPTPATH}/.${VAR_THING}/bin/start.sh" > "${VAR_CONF_DIR}/bin/start.sh"
 
 sed -e "s|<VAR_SRC_DIR>|${VAR_SRC_DIR}|g" \
   -e "s|<VAR_CONF_DIR>|${VAR_CONF_DIR}|g" \
   -e "s|<VAR_CONF_FILE>|${VAR_CONF_FILE}|g" \
   -e "s|<VAR_USERNAME>|${VAR_USERNAME}|g" \
+  -e "s|<VAR_THING>|${VAR_THING}|g" \
   "${SCRIPTPATH}/.${VAR_THING}/bin/stop.sh" > "${VAR_CONF_DIR}/bin/stop.sh"
 
 sed -e "s|<VAR_CONF_FILE>|${VAR_CONF_FILE}|g" \
+  -e "s|<VAR_THING>|${VAR_THING}|g" \
   "${SCRIPTPATH}/.${VAR_THING}/bin/healthcheck.sh" > "${VAR_CONF_DIR}/bin/healthcheck.sh"
 
 sed -e "s|<VAR_SRC_DIR>|${VAR_SRC_DIR}|g" \
+  -e "s|<VAR_THING>|${VAR_THING}|g" \
   "${SCRIPTPATH}/.${VAR_THING}/bin/status.sh" > "${VAR_CONF_DIR}/bin/status.sh"
 
 # Symlink binaries
