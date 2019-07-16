f63f599ffebacf7d431bb9f4d25d8b6f4eb3bf96
ns408 <nsrea408@gmail.com>
2019-07-17 11:16:43 +1200

    end of season

--- bin/misc/stop_raw.sh
+++ bin/misc/stop_raw.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
 sudo /etc/init.d/monit stop
-~/.bitcoin/bin/stop.sh
 ~/.komodo/bin/stop.sh
-~/.komodo/bin/ac_stop.sh
 ~/.chips/bin/stop.sh
 ~/.gamecredits/bin/stop.sh
 ~/.komodo/VRSC/bin/stop.sh
-~/.hush/bin/stop.sh
 ~/.einsteinium/bin/stop.sh
 ${HOME}/.gincoincore/bin/stop.sh
 
-#pkill -15 iguana
+pkill -15 iguana
