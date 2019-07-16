06e78d82681589e366cc5911aeaa6bfc5026a732
ns408 <nsrea408@gmail.com>
2019-07-17 08:46:52 +1000

    removing daemons from start_raw.sh
    Removing other assest chains and BTC because of end of the season.

--- bin/misc/start_raw.sh
+++ bin/misc/start_raw.sh
@@ -13,24 +13,18 @@ echo -e "# Starting all services\n"
 
 cd ${HOME}/misc_scripts
 
-${HOME}/.bitcoin/bin/start.sh &
 ${HOME}/.komodo/bin/start.sh &
-${HOME}/.komodo/bin/ac_start.sh &
 ${HOME}/.chips/bin/start.sh &
 ${HOME}/.gamecredits/bin/start.sh &
 ${HOME}/.komodo/VRSC/bin/start.sh &
-${HOME}/.hush/bin/start.sh &
 ${HOME}/.einsteinium/bin/start.sh &
 ${HOME}/.gincoincore/bin/start.sh &
 
 
-${HOME}/.bitcoin/bin/status.sh
 ${HOME}/.komodo/bin/status.sh
-${HOME}/.komodo/bin/ac_status.sh
 ${HOME}/.chips/bin/status.sh
 ${HOME}/.gamecredits/bin/status.sh
 ${HOME}/.komodo/VRSC/bin/status.sh
-${HOME}/.hush/bin/status.sh
 ${HOME}/.einsteinium/bin/status.sh
 ${HOME}/.gincoincore/bin/status.sh
 
